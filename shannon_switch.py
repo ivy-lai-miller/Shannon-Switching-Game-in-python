46f0e12ba3ac0617c254a2510e7ee3aaef628d91
ivy-lai-miller <il227@cornell.edu>
2017-05-05 17:04:19 -0400

    commented and ready for submission

--- shannon_switch.py
+++ shannon_switch.py
@@ -21,7 +21,7 @@ class Game:
         for line in lines:
             line = line.split()
 
-            #check for blank lines and skip
+            # Check for blank lines and skip
             if len(line) ==0:
                 continue
 
@@ -46,6 +46,7 @@ class Game:
                         print "Link is specified incorrectly."
                         sys.exit(1)
 
+            # Store which nodes are special
             if line[0] == "S":
                 if len(line) < 3:
                     print "Error in specifying special nodes."
@@ -55,14 +56,15 @@ class Game:
                     self.special.append(int(line[2]))
 
 
+        # Print if there is an error in specifying the game
         if self.num_points == None or self.num_points == 0:
             print "Points not specified"
             sys.exit(1)
         if len(self.special) < 2:
-            print "Special Nodes not defined."
+            print "Special Nodes not specified."
             sys.exit(1)
         if len(self.links) ==0:
-            print "No links are defined."
+            print "No links are specified."
             sys.exit(1)
 
     # Print function for debugging
@@ -71,6 +73,8 @@ class Game:
         print "Number of links: %d" %len(self.links)
         print "Special Nodes are: %d, %d" %(self.special[0], self.special[1])
 
+
+    # Run function to play the game
     def run(self):
         # Set up the networkx graph/board
         G = nx.MultiGraph()
@@ -78,11 +82,13 @@ class Game:
         G.add_edges_from(self.links)
         status = None
 
+        # Color all nodes blue except for special nodes which are red
         for node in G.node:
             # red nodes are special
             if node not in self.special:
                 G.node[node]["fill"] = "blue"
 
+        # Designate which mode Player 1 will be
         first = raw_input("Who do you want to go first? Type SHORT or CUT>> ")
         if first.upper() == "SHORT":
             status = "SHORT"
@@ -94,66 +100,82 @@ class Game:
 
         playing = True
 
+        # Once user has chosen the mode, start playing the game
         while playing:
 
+            # Display the network using NetworkX Viewer for each loop
             Viewer(G).mainloop()
 
+            # Display text to user and prompt for a move
+            # the user input is stored in raw
             if status == "SHORT":
                 print "\nSHORT's move. Type EXIT to end. a b to short the edge between a and b."
                 raw = raw_input("What would you like to do? ")
-                # pass
 
             if status == "CUT":
                 print "\nCUT's move. Type EXIT to end. a b to delete the edge between a and b."
                 raw = raw_input("What would you like to do? ")
-                # pass
 
+            # option to exit the game
             if raw.upper() == "EXIT":
                 playing = False
                 continue
 
+            # option to display the board if the user forgets or closes by accident
             if raw.upper() == "SHOW BOARD":
                 continue
 
 
-                # check if a move is valid
+            # play with the user input, which should be two integers
             else:
                 if status == "SHORT":
-                # If you SHORT, the  two points merge
-                # Update connections
-                # Make sure you also update the status now to SHORT
-                    c, d = map(int, raw.split())
+
                     try:
+                        # maps the user input to two integers c,d
+                        c, d = map(int, raw.split())
+
+                        # switch the two values to keep the special node
+                        # This is because contracted edge technically merges second node into the first node
                         if d in self.special:
                             c,d = d,c
-                        # contracted_edge
-                        G=nx.contracted_edge(G,(c,d))
-                        if (c==self.special[0] and d==self.special[1]) or (c==self.special[1] and d==self.special[0]):
+
+                        # contracted_edge(network, location as tuple)
+                        G = nx.contracted_edge(G,(c,d))
+
+                        # If the code gets to this point, that means c and d are indeed connected
+                        # Then we should see if we shorted the link between two special points
+                        # If so, then we win!
+                        if c in self.special and d in self.special:
                             print "Short wins"
                             playing=False
+
+                        # Otherwise, it's the next player's turn
                         else:
                             status= "CUT"
-                            continue
-                        # print "if there is an exception this shouldnt print"
+                        continue
+                    # If anything fails, then catch exception
                     except:
                         print "Bad input!"
 
                 if status == "CUT":
                     # If you cut, the connection is gone
-                    # Make sure you also update the status now to SHORT
 
-                    a, b = map(int, raw.split()) #this works there just has to be a space between the two numbers
                     try:
+                        a, b = map(int, raw.split()) #this works there just has to be a space between the two numbers
                         G.remove_edge(a,b)
+
+                        # Check if there is any path between the two special points
+                        # If not, cut wins!
                         if (nx.has_path(G,self.special[0],self.special[1])==False):
                             print "Cut wins"
                             playing= False
+
                         else:
+                            # Next player's move
                             status="SHORT"
-                            continue
+                        continue
 
                     except:
-                        # continue
                         print "Bad input!"
 
 
